074b022b4aa5eb68f3f5ce911d10f78ffa67df11
kyoubuntu <shining9lee@gmail.com>
2025-04-30 23:17:23 +0900

    kyoubuntu

--- kyouhele/main.cpp
+++ kyouhele/main.cpp
@@ -1,67 +1,73 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// void bsort(int *arr, int size);
-
-
-// int main()
-// {
-// 	char input[11];
-// 	scanf("%s", input);
-// 	int len = 0;
-// 	while (input [len])
-// 		len++;
-	
-// 	int numarr[len];
-// 	int i = 0;
-// 	while (i < len)
-// 	{
-// 		numarr[i] = input[i] - '0';
-// 		if (input[i] == '0')
-// 			numarr[i] = 0;
-// 		i++;
-// 	}
-// 	bsort(numarr, i);
-// 	i = 0;
-// 	while(i < len)
-// 	{
-// 		printf("%d",	numarr[i]);
-// 		i++;
-// 	}
-// 	return 0;
-// }
-
-// void bsort(int *arr, int size)
-// {
-// 	int idx = 0, j = 0;
-// 	while (idx < size)
-// 	{
-// 		j = 0;
-// 		while (j < size - idx - 1)
-// 		{
-// 			if (arr[j] < arr[j+1])
-// 			{
-// 				int tmp = arr[j];
-// 				arr[j] = arr[j + 1];
-// 				arr[j + 1] = tmp;
-// 			}
-// 			j++;
-// 		}
-// 		idx++;
-// 	}
-// }
-
 #include <iostream>
 #include <algorithm>
 using namespace std;
+
+struct primeN
+{
+	int num;
+	int first;
+	int second;
+};
+
+primeN find_prime(primeN num);
+bool is_prime(int num);
 
 
 int main ()
 {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
-	string num;
-	cin >>num;
-	sort(num.begin(), num.end(), greater<char>());
-	cout << num;
+	int n, i = 0;
+	cin >> n;
+
+	primeN *num = new primeN[n];
+	while (i < n)
+	{
+		cin >> num[i].num;
+		if (num[i].num % 2)
+			return 0;
+		num[i] = find_prime(num[i]);
+		// cout << num[i].first <<" -------- "<< num[i].second << endl;			//출력부
+		i++;
+	}
+
+	i = 0;
+	while (i < n)
+	{
+		cout << num[i].first <<" "<< num[i++].second << endl;			//출력부
+	}
+	delete[] num;
 	return 0;
+}
+
+primeN find_prime(primeN input)
+{
+	int small =  input.num/2, big = input.num - small;
+	while (1)
+	{
+		if (is_prime(small) && is_prime(big))
+		{
+			input.first = small;
+			input.second = big;
+			break;
+		}
+		small--;
+		big++;
+	}
+	// cout << input.first <<"========== "<< input.second << endl;			//출력부
+	return input;
+}
+
+bool is_prime(int num)
+{
+	int i = 2;
+
+	while (i < num)
+	{
+		if (num % i == 0)
+			return (false);
+		i++;
+	}
+	return (true);
+
 }
